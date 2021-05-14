e868e71c39afb2f5eb75940eea4cfedf8567ecd3
Octavia Togami <octavia.togami@gmail.com>
2021-05-14 12:13:49 -0700

    Add a little more debug

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process::exit;
 use anyhow::{anyhow, Context, Result};
 use colored::*;
 use either::Either;
+use log::{debug, info};
 use structopt::StructOpt;
 
 use crate::config::Configuration;
@@ -123,7 +124,7 @@ fn main() {
 fn main_for_result(args: Jpre) -> Result<()> {
     let mut config = Configuration::new().context("Failed to load config")?;
     stderrlog::new()
-        .verbosity(args.verbose)
+        .verbosity(args.verbose + 1)
         .init()
         .context("Failed to initialize logging")?;
     match args.cmd {
@@ -144,8 +145,10 @@ fn main_for_result(args: Jpre) -> Result<()> {
 
             for major in majors {
                 if let Some((_, version)) = versions.iter().filter(|(x, _)| *x == major).next() {
+                    info!("Checking latest version of {}", version);
                     let latest = adoptjdk::get_latest_jdk_version(major)
                         .context("Failed to get latest JDK version")?;
+                    debug!("Latest version of {} is {}", version, latest);
                     if latest != *version {
                         println!(
                             "{} {}",
@@ -170,6 +173,7 @@ fn main_for_result(args: Jpre) -> Result<()> {
 
             if !check {
                 for major in update_versions {
+                    info!("Updating to latest version of {}", major);
                     jdk_manager::update_jdk(major).context("Failed to update JDK")?;
                 }
             }
